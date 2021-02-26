e678b79e1fab9ed9ebcab0ca79e5342f8f511f53
KubaBombel1 <79452843+KubaBombel1@users.noreply.github.com>
2021-02-26 21:58:47 +0100

    Add files via upload

---
+++ Zadanie2.java
@@ -0,0 +1,19 @@
+
+package zadanie.pkg2;
+
+
+public class Zadanie2 {
+
+    
+    public static void main(String[] args) {
+        
+        double wynik;
+        
+        wynik = (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5); 
+        
+        System.out.println(wynik);
+                
+   
+    }
+    
+}
