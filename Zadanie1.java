b05bcde5f094d3e2a75f3bf38ef47a1df299ef47
KubaBombel1 <79452843+KubaBombel1@users.noreply.github.com>
2021-02-26 21:53:21 +0100

    Add files via upload

---
+++ Zadanie1.java
@@ -0,0 +1,13 @@
+
+package zadanie.pkg1;
+
+public class Zadanie1 {
+    
+    public static void main(String[] args) {
+       
+       System.out.println ("Ania");
+       System.out.println ("Bartek");
+       System.out.println ("Kasia");
+    }
+    
+}
